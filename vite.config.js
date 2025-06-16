a1ec1aa942c2acaa1fb19bb34b79c66357fcc383
Márcio Carvalho de Barros Neto <marcio.neto@detran.local>
2025-06-16 14:58:37 -0400

    Initial clean commit of frontend application

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Configurações para o build:
+  base: '/',          // Define a URL base para a aplicação.
+  root: '.',          // A raiz do seu projeto Vite é o diretório atual do repositório clonado.
+  publicDir: 'public', // A pasta que contém o index.html e outros assets estáticos.
+  build: {
+    outDir: 'dist',    // O diretório de saída para os arquivos compilados.
+  },
+})
